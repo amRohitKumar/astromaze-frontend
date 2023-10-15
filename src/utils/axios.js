302fcd511d0c669b5cf04f3c9899c09b5102ff62
Rohit Kumar <shutupRohit@gmail.com>
2023-10-15 14:50:42 +0530

    add constraint

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const customFetch = axios.create({
-  // baseURL: "https://astromaze.onrender.com",
-  baseURL: "http://localhost:8080",
+  baseURL: "https://astromaze.onrender.com",
+  // baseURL: "http://localhost:8080",
   headers: {
     "Content-Type": "application/json",
   },
